100c29b0cc89e386620e3dce354d0ed0423bdb59
Artur Ciepielowski <artciepi@gmail.com>
2023-03-12 19:17:00 +0100

    Added fake character selection screen

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -23,29 +23,65 @@ public class KeyHandler implements KeyListener {
         int code = e.getKeyCode();
         //TITLE STATE
         if (gp.gameState == gp.titleState) {
+            if (gp.ui.titleScreenState == 0) {
+                if (code == KeyEvent.VK_W) {
+                    gp.ui.comandNum--;
+                    if (gp.ui.comandNum < 0) {
+                        gp.ui.comandNum = 2;
+                    }
+                }
+                if (code == KeyEvent.VK_S) {
+                    gp.ui.comandNum++;
+                    if (gp.ui.comandNum > 2) {
+                        gp.ui.comandNum = 0;
+                    }
+                }
+                if (code == KeyEvent.VK_ENTER) {
+                    if (gp.ui.comandNum == 0) {
+                        gp.ui.titleScreenState = 1;
 
-            if (code == KeyEvent.VK_W) {
-                gp.ui.comandNum--;
-                if (gp.ui.comandNum < 0) {
-                    gp.ui.comandNum = 2;
+                    }
+                    if (gp.ui.comandNum == 1) {
+                        //add later
+                    }
+                    if (gp.ui.comandNum == 2) {
+                        System.exit(0);
+                    }
                 }
             }
-            if (code == KeyEvent.VK_S) {
-                gp.ui.comandNum++;
-                if (gp.ui.comandNum > 2) {
-                    gp.ui.comandNum = 0;
+
+            else if (gp.ui.titleScreenState == 1) {
+                if (code == KeyEvent.VK_W) {
+                    gp.ui.comandNum--;
+                    if (gp.ui.comandNum < 0) {
+                        gp.ui.comandNum = 3;
+                    }
                 }
-            }
-            if(code ==KeyEvent.VK_ENTER){
-                if(gp.ui.comandNum==0){
-                    gp.gameState=gp.playState;
-                    gp.playMusic(0);
+                if (code == KeyEvent.VK_S) {
+                    gp.ui.comandNum++;
+                    if (gp.ui.comandNum > 3) {
+                        gp.ui.comandNum = 0;
+                    }
                 }
-                if(gp.ui.comandNum==1){
-                    //add later
-                }
-                if(gp.ui.comandNum==2){
-                    System.exit(0);
+                if (code == KeyEvent.VK_ENTER) {
+                    if (gp.ui.comandNum == 0) {
+                        System.out.println("Do some Lina stuff");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.comandNum == 1) {
+                        System.out.println("Do some Zelgradis stuff");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.comandNum == 2) {
+                        System.out.println("Do some Amelia stuff");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.comandNum == 3) {
+                        gp.ui.titleScreenState = 0;
+                    }
                 }
             }
         }
--- src/main/UI.java
+++ src/main/UI.java
@@ -16,7 +16,8 @@ public class UI {
     int messageCounter = 0;
     public boolean gameFinished = false;
     public String currentDialogue = "";
-public int comandNum =0;
+    public int comandNum = 0;
+    public int titleScreenState = 0; // 0 the first screen 1: the second screen
 
     public UI(GamePanel gp) {
         this.gp = gp;
@@ -62,62 +63,101 @@ public int comandNum =0;
     }
 
     public void drawTitleScreen() {
-        g2.setColor(new Color(0, 0, 0));
-        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
-        //TITLE NAME
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
-        String text = "Slayers";
-        int x = getXforCenteredText(text);
-        int y = gp.tileSize * 3;
-        //SHADOW
-        g2.setColor(Color.gray);
-        g2.drawString(text, x+4, y+4);
-        //MAIN COLOR
-        g2.setColor(Color.yellow);
-        g2.drawString(text, x, y);
+        if (titleScreenState == 0) {
+            g2.setColor(new Color(0, 0, 0));
+            g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+            //TITLE NAME
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
+            String text = "Slayers";
+            int x = getXforCenteredText(text);
+            int y = gp.tileSize * 3;
+            //SHADOW
+            g2.setColor(Color.gray);
+            g2.drawString(text, x + 4, y + 4);
+            //MAIN COLOR
+            g2.setColor(Color.yellow);
+            g2.drawString(text, x, y);
 
 
-        String subText = "Xellos lost stories";
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 56F));
-        x = getXforCenteredText(subText);
+            String subText = "Xellos lost stories";
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 56F));
+            x = getXforCenteredText(subText);
 
-        //SHADOW
-        g2.setColor(new Color(84,68,135));
-        g2.drawString(subText, x+4, y + 99);
-        //MAIN COLOR
-        g2.setColor(new Color(196,179,228));
-        g2.drawString(subText, x, y + 96);
+            //SHADOW
+            g2.setColor(new Color(84, 68, 135));
+            g2.drawString(subText, x + 4, y + 99);
+            //MAIN COLOR
+            g2.setColor(new Color(196, 179, 228));
+            g2.drawString(subText, x, y + 96);
 
-        //XELLOS CHARACTER
-        g2.setColor(Color.white);
-        x=gp.screenWidth/2-(gp.tileSize*2)/2;
-        y+=gp.tileSize*2+10;
-        g2.drawImage(gp.xellos.down1,x,y,gp.tileSize*2,gp.tileSize*2,null);
+            //XELLOS CHARACTER
+            g2.setColor(Color.white);
+            x = gp.screenWidth / 2 - (gp.tileSize * 2) / 2;
+            y += gp.tileSize * 2 + 10;
+            g2.drawImage(gp.xellos.down1, x, y, gp.tileSize * 2, gp.tileSize * 2, null);
 
-        //MENU
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 48F));
-        text="NEW GAME";
-        x=getXforCenteredText(text);
-        y+=gp.tileSize*3.5;
-        g2.drawString(text,x,y);
-        if(comandNum ==0){
-            g2.drawString(">",x-gp.tileSize,y);
-        }
+            //MENU
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 48F));
+            text = "NEW GAME";
+            x = getXforCenteredText(text);
+            y += gp.tileSize * 3.5;
+            g2.drawString(text, x, y);
+            if (comandNum == 0) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
 
-        text="LOAD GAME";
-        x=getXforCenteredText(text);
-        y+=gp.tileSize;
-        g2.drawString(text,x,y);
-        if(comandNum ==1){
-            g2.drawString(">",x-gp.tileSize,y);
-        }
+            text = "LOAD GAME";
+            x = getXforCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (comandNum == 1) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
 
-        text="QUIT";
-        x=getXforCenteredText(text);
-        y+=gp.tileSize;
-        g2.drawString(text,x,y);
-        if(comandNum ==2){
-            g2.drawString(">",x-gp.tileSize,y);
+            text = "QUIT";
+            x = getXforCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (comandNum == 2) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
+        } else if (titleScreenState == 1) {
+            //CHARACTER SELECTION SCREEN
+            g2.setColor(Color.white);
+            g2.setFont(g2.getFont().deriveFont(42F));
+            String text = "Select your hero!";
+            int x=getXforCenteredText(text);
+            int y=gp.tileSize*3;
+            g2.drawString(text,x,y);
+
+            text="Lina Inverse";
+            x= getXforCenteredText(text);
+            y+=gp.tileSize*3;
+            g2.drawString(text,x,y);
+            if(comandNum==0){
+                g2.drawString(">",x-gp.tileSize,y);
+            }
+            text="Zelgadiss Graywords";
+            x= getXforCenteredText(text);
+            y+=gp.tileSize;
+            g2.drawString(text,x,y);
+            if(comandNum==1){
+                g2.drawString(">",x-gp.tileSize,y);
+            }
+            text="Amelia Wil Tesla Seyruun";
+            x= getXforCenteredText(text);
+            y+=gp.tileSize;
+            g2.drawString(text,x,y);
+            if(comandNum==2){
+                g2.drawString(">",x-gp.tileSize,y);
+            }
+            text="Back";
+            x= getXforCenteredText(text);
+            y+=gp.tileSize*2;
+            g2.drawString(text,x,y);
+            if(comandNum==3){
+                g2.drawString(">",x-gp.tileSize,y);
+            }
         }
 
     }
