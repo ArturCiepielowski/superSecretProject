d189d41d0f21d160230a7f7d8722c4c1edf75314
Artur Ciepielowski <artciepi@gmail.com>
2023-03-27 00:17:30 +0200

    Added particles

--- src/object/OBJ_Rock.java
+++ src/object/OBJ_Rock.java
@@ -5,6 +5,8 @@ import entity.Entity;
 import entity.Projectile;
 import main.GamePanel;
 
+import java.awt.*;
+
 public class OBJ_Rock extends Projectile {
     GamePanel gp;
 
@@ -44,5 +46,21 @@ public class OBJ_Rock extends Projectile {
 
     public void substractResource(Entity user){
         user.ammo -= useCost;
+    }
+    public Color getParticleColor(){
+        Color color = new Color(40,50,0);
+        return color;
+    }
+    public int getParticleSize(){
+        int size=10; // 6 pixels
+        return  size;
+    }
+    public int getParticleSpeed(){
+        int speed =1;
+        return speed;
+    }
+    public int getParticleMaxLife(){
+        int maxLife=20;
+        return maxLife;
     }
 }
