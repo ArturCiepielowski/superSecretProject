e8a68613a750a75817b5e6556bbb005cbfed36f1
Artur Ciepielowski <artciepi@gmail.com>
2023-04-18 22:11:33 +0200

    Begining of lighting efect

---
+++ src/enviroment/EnviromentManager.java
@@ -0,0 +1,20 @@
+package enviroment;
+
+import main.GamePanel;
+
+import java.awt.*;
+
+public class EnviromentManager {
+    GamePanel gp;
+    Lighting lighting;
+
+    public EnviromentManager(GamePanel gp){
+        this.gp=gp;
+    }
+    public void setup(){
+        lighting = new Lighting(gp, 350);
+    }
+    public void draw(Graphics2D g2){
+        lighting.draw(g2);
+    }
+}
