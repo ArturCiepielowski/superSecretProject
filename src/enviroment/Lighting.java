c7b21226441168951262115bbe02c224a1669e41
Artur Ciepielowski <artciepi@gmail.com>
2023-04-19 23:20:07 +0200

    Day night cycle

--- src/enviroment/Lighting.java
+++ src/enviroment/Lighting.java
@@ -11,6 +11,14 @@ import java.awt.image.BufferedImage;
 public class Lighting {
     GamePanel gp;
     BufferedImage darknessFilter;
+    int dayCounter;
+    float filterAlpha =0f;
+    //Day state
+    final int day=0;
+    final int dusk=1;
+    final int night=2;
+    final int dawn=3;
+    int dayState=day;
 
     public Lighting(GamePanel gp){
         this.gp=gp;
@@ -80,9 +88,52 @@ public class Lighting {
             setLightSource();
             gp.player.lightUpdated=false;
         }
+        //Check teh state of the day
+        if(dayState==day){
+            dayCounter++;
+            if(dayCounter>600){
+                dayState = dusk;
+                dayCounter=0;
+            }
+        }
+        if(dayState==dusk){
+            filterAlpha+=0.001f;
+            if(filterAlpha >1f){
+                filterAlpha=1f;
+                dayState=night;
+            }
+        }
+        if(dayState ==night){
+            dayCounter++;
+
+            if(dayCounter>600){
+                dayState=dawn;
+                dayCounter=0;
+            }
+        }
+        if(dayState ==dawn){
+            filterAlpha-=0.001f;
+            if(filterAlpha <0f){
+                filterAlpha =0;
+                dayState=day;
+            }
+        }
     }
     public void draw(Graphics2D g2){
-
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,filterAlpha));
         g2.drawImage(darknessFilter,0,0,null);
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1f));
+
+        //DEBUG
+        String situation ="";
+        switch(dayState){
+            case day: situation="Day";break;
+            case dusk: situation="Dusk";break;
+            case night: situation="Night";break;
+            case dawn: situation="Dawn";break;
+        }
+        g2.setColor(Color.white);
+        g2.setFont(g2.getFont().deriveFont(40f));
+        g2.drawString(situation,800,500);
     }
 }
